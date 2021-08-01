e969892c8455c7963c4e68d4f343d452f643b8bd
Hugo Mercier <mercier.hugo14@gmail.com>
2021-08-01 19:29:53 +0200

    creation liste produits

---
+++ hoopop-projet/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>Liste des produits</h1>
+    <button type="button">Valider le panier</button>
+    <br>
+    <li class="liste" v-for="product in products" :key="product.productId">
+      <img class="pic" :src="product.productPicture">
+      {{product.productName}}
+      
+      <button>+</button>
+    </li>
+    
+  </div>
+</template>
+
+<script>
+
+import productsData from './products.json'
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  data() {
+    return {
+      products: productsData,
+    };
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.liste {
+  text-align: left;
+  padding-top: 20px;
+  list-style: none;
+  display: inline-block
+}
+.pic {
+  height: 100px;
+  padding: 20px;
+  vertical-align: middle;
+}
+</style>
